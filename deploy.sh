9ee28d0931ab62c368883e4a6562080d46aed299
theOppermanian <theOppermanian@oppermania.com>
2020-04-01 17:54:42 -0400

    added comment

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ docker build -t theoppermanian/multi-client:latest -t theoppermanian/multi-clien
 docker build -t theoppermanian/multi-server:latest -t theoppermanian/multi-server:$GIT_SHA -f ./server/Dockerfile ./client
 docker build -t theoppermanian/multi-worker:latest -t theoppermanian/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./client
 
-docker push theoppermanian/multi-client:lastest
+docker push theoppermanian/multi-client:latest
 docker push theoppermanian/multi-client:$GIT_SHA
 docker push theoppermanian/multi-server:latest
 docker push theoppermanian/multi-server:$GIT_SHA
